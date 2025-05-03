f5fd205ff46e243f7f07572a9b3977c182569613
Vitor Rodrigues <vitorrodrys@gmail.com>
2025-05-03 14:46:00 -0300

    refactor: move error handling to RarviApp and clean up login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,116 @@
 import 'package:flutter/material.dart';
 
+import 'package:rarvi/services/api/rarvi_api.dart';
+
 import 'package:rarvi/view/home/home.dart';
 import 'package:rarvi/view/login/login.dart';
 import 'package:rarvi/view/login/recovery_password.dart';
 import 'package:rarvi/view/login/signup.dart';
 
 
+class RarviApp extends StatelessWidget {
+
+  final RarviAPI _api = RarviAPI();
+
+  final Widget _child;
+  RarviApp({super.key, required Widget child}) 
+    : _child = child;
+
+  void _desconect_handler(BuildContext context, APIErrorEnum unloggedError){
+    switch (unloggedError) {
+      case APIErrorEnum.tokenExpired:
+        Navigator.of(context).pushNamedAndRemoveUntil('/login', (route) => false);
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Sessão expirada"),
+            backgroundColor: Colors.red,
+            duration: Duration(seconds: 10),
+          ),
+        );
+        break;
+      case APIErrorEnum.loggedOut:
+        Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false);
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Desconectado"),
+            backgroundColor: Colors.red,
+            duration: Duration(seconds: 10),
+          ),
+        );
+        break;
+      case APIErrorEnum.unauthorized:
+        Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false);
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Usuário não autorizado"),
+            backgroundColor: Colors.red,
+            duration: Duration(seconds: 10),
+          ),
+        );
+        break;
+      default:
+        break;
+    }
+  }
+
+  void _connection_error_handler(BuildContext context, APIErrorEnum error){
+    switch (error) {
+      case APIErrorEnum.timeout:
+      case APIErrorEnum.connectionError:
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Erro de conexão, tente novamente mais tarde"),
+            backgroundColor: Colors.red,
+            duration: Duration(seconds: 10),
+          ),
+        );
+        break;
+      default:
+        break;
+    }
+  }
+
+  /**
+   * This method is just a wrapper to add the session listeners to the API
+   * and redirect the user to the login screen.
+   */
+  @override
+  Widget build(BuildContext context) {
+    _api.addSessionListener(
+      'desconetHandler',
+      (error) => _desconect_handler(context, error),
+      [
+        APIErrorEnum.tokenExpired,
+        APIErrorEnum.loggedOut,
+        APIErrorEnum.unauthorized,
+      ]
+    );
+    _api.addSessionListener(
+      'connectionErrorHandler',
+      (error) => _connection_error_handler(context, error),
+      [
+        APIErrorEnum.timeout,
+        APIErrorEnum.connectionError,
+      ]
+    );
+    return _child;
+  }
+}
+
 void main() {
   runApp(
     MaterialApp(
-      initialRoute: '/',
+      title: 'Rarvi',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/login',
       routes: {
-        '/': (context) => const LoginScreen(),
+        '/login': (context) => RarviApp(child: const LoginScreen()),
         '/signup': (context) => const SignUpScreen(),
         '/RecoveryPassword': (context) => const RecoveryScreen(),
         '/home': (context) => const HomeScreen(),
-      }, 
+      },
     )
   );
 }
