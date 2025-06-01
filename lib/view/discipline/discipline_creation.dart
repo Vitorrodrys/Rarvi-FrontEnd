6efd10e46ef28f0fe1cc4cc1851666d4895f0e49
Vitor <vitorrodrys@gmail.com>
2025-06-01 16:20:53 -0300

    feat: add discipline creation screen and route

---
+++ lib/view/discipline/discipline_creation.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:rarvi/services/api/rarvi_api.dart';
+
+class DisciplineCreationScreen extends StatefulWidget {
+  const DisciplineCreationScreen({super.key});
+
+  @override
+  State<DisciplineCreationScreen> createState() => _DisciplineCreationState();
+}
+
+class _DisciplineCreationState extends State<DisciplineCreationScreen> {
+
+  final RarviAPI _api = RarviAPI();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox();
+  }
+
+}
